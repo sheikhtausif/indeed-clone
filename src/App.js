9e358da92b7af81601703c2ca6fce765af69ee72
Mohd Tausif <mohammadtausif14@gmail.com>
2021-07-24 19:37:22 +0530

    push is master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import MainApply from './Components/ApplyJobs/MainApply'
-import { Mypage } from './Components/Login&SignIn/Mypage'
+// import { Mypage } from './Components/Login&SignIn/Mypage'
 
 function App() {
   return (
     <div className="App">
       <MainApply />
-      <h1><Mypage /></h1>
+      {/* <h1><Mypage /></h1> */}
 
     </div>
   );
