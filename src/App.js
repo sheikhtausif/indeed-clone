cf8dddb6abad755442e8a62cab15552c39c9e7f6
Shariq Ansari <ansarisharique0003@gmail.com>
2021-07-24 11:44:55 +0530

    signIn Sing up static page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import {Mypage} from './components/Login&SignIn/Mypage'
 function App() {
   return (
     <div className="App">
-      
+      <h1><Mypage /></h1>
     </div>
   );
 }
