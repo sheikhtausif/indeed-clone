9bc32952ca05b4f0bd0a13ebb664fedf8c36be76
Mohd Tausif <mohammadtausif14@gmail.com>
2021-07-24 19:42:38 +0530

    merge code

--- src/Components/ApplyJobs/MainApply.jsx
+++ src/Components/ApplyJobs/MainApply.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import ApplyForm from './ApplyForm'
-// import { useLocation } from 'react-router'
 
 const MainApplyDiv = styled.div`
     width: 100%;
